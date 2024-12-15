ac0eedc555c296bcd95f7ac8773c3640ffd8cf15
rakshitnayak <rdsonnn@gmail.com>
2024-12-15 14:57:26 +0530

    Update packages

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const  userSchema = require("./schemas/user");
+const userSchema = require("./schemas/user");
 const postSchema = require("./schemas/post");
 
 const app = express();
@@ -16,6 +16,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.use(
   session({
     name: "sid",
@@ -23,83 +24,90 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 86400,
-      sameSite: true,
+      maxAge: 86400000, // 24 hours
+      sameSite: "strict",
     },
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-//1st step to connect mongoose to application after requiring mongoose.
-let uri = `mongodb+srv://users:${process.env.DB_PW}@cluster0.6kynm.mongodb.net/`;
+// Mongoose Connection
+const uri = `mongodb+srv://users:${process.env.DB_PW}@cluster0.6kynm.mongodb.net/?retryWrites=true&w=majority`;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Connection error:", err));
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
+// Add Passport-Local Mongoose plugin to the user schema
 userSchema.plugin(passportLocalMongoose);
 
-const User = new mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 const Post = mongoose.model("Post", postSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => done(null, user))
+    .catch((err) => done(err));
+});
 
-app.get("/", function (req, res) {
+app.get("/", async (req, res) => {
   const { userId } = req.session;
-  let loginUserId;
-  if (req.isAuthenticated() && userId) {
-    loginUserId = userId;
-  }
-  Post.find({}, function (err, posts) {
+  const loginUserId = req.isAuthenticated() ? userId : null;
+  try {
+    const posts = await Post.find({});
     res.render("home", {
-      posts: posts,
+      posts,
       loginUserId,
     });
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching posts.");
+  }
 });
 
-app.get("/posts", function (req, res) {
-  res.render("post", {});
+app.get("/posts", (req, res) => {
+  res.render("post");
 });
 
-app.get("/articles", function (req, res) {
-  res.render("post", {});
+app.get("/articles", (req, res) => {
+  res.render("post");
 });
 
-app.get("/login", function (req, res) {
+app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("/register", function (req, res) {
+app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.get("/compose", function (req, res) {
+app.get("/compose", (req, res) => {
   const { userId } = req.session;
   if (req.isAuthenticated()) {
-    res.render("compose", { userId: userId });
+    res.render("compose", { userId });
   } else {
     res.redirect("/login");
   }
 });
 
-app.post("/register", function (req, res) {
+app.post("/register", (req, res) => {
   User.register(
     { username: req.body.username },
     req.body.password,
-    function (err, user) {
+    (err, user) => {
       if (err) {
-        console.log(err);
+        console.error(err);
         res.redirect("/register");
       } else {
-        passport.authenticate("local")(req, res, function () {
-          req.session.userId = req.user.username;
+        passport.authenticate("local")(req, res, () => {
+          req.session.userId = user.username;
           res.redirect("/compose");
         });
       }
@@ -107,17 +115,17 @@ app.post("/register", function (req, res) {
   );
 });
 
-app.post("/login", function (req, res) {
+app.post("/login", (req, res) => {
   const user = new User({
     username: req.body.username,
     password: req.body.password,
   });
 
-  req.login(user, function (err) {
+  req.login(user, (err) => {
     if (err) {
-      console.log(err);
+      console.error(err);
     } else {
-      passport.authenticate("local")(req, res, function () {
+      passport.authenticate("local")(req, res, () => {
         req.session.userId = req.user.username;
         res.redirect("/compose");
       });
@@ -125,7 +133,7 @@ app.post("/login", function (req, res) {
   });
 });
 
-app.post("/compose", function (req, res) {
+app.post("/compose", async (req, res) => {
   const post = new Post({
     image: req.body.postImage,
     title: req.body.postTitle,
@@ -134,32 +142,40 @@ app.post("/compose", function (req, res) {
     date: req.body.postDate,
   });
 
-  post.save(function (err) {
-    if (!err) {
-      res.redirect("/");
-    }
-  });
+  try {
+    await post.save();
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error saving post.");
+  }
 });
 
-app.get("/posts/:postId", function (req, res) {
+app.get("/posts/:postId", async (req, res) => {
   const requestedPostId = req.params.postId;
 
-  Post.findOne({ _id: requestedPostId }, function (err, post) {
-    res.render("post", {
-      image: post.image,
-      title: post.title,
-      content: post.content,
-      author: post.author,
-      date: post.date,
-    });
-  });
+  try {
+    const post = await Post.findById(requestedPostId);
+    if (post) {
+      res.render("post", {
+        image: post.image,
+        title: post.title,
+        content: post.content,
+        author: post.author,
+        date: post.date,
+      });
+    } else {
+      res.status(404).send("Post not found.");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching post.");
+  }
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+// Dynamic Port Handling
+const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log("Server listening on Port:", `http://localhost:${port}`);
 });
